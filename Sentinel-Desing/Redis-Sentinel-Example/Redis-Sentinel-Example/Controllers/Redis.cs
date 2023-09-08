c6d707c1015940a00f33b766b160d52278ac69d7
Oguzhankaraguzel <oguzhan.karaguzel@gmail.com>
2023-09-08 04:21:23 +0300

    Sentinel desing example

---
+++ Sentinel-Desing/Redis-Sentinel-Example/Redis-Sentinel-Example/Controllers/Redis.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis_Sentinel_Example.Services;
+
+namespace Redis_Sentinel_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Redis : ControllerBase
+    {
+        [HttpGet("[action]/{key}/{value}")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            var redis = await RedisService.RedisMasterDatabase();
+            try
+            {
+                await redis.StringSetAsync(key, value);
+                return CreatedAtAction(nameof(SetValue), new { Key = key, Value = value });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error : " + ex.Message);
+            }
+
+
+        }
+        [HttpGet("[action]/{key}")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            var redis = await RedisService.RedisMasterDatabase();
+            var data = await redis.StringGetAsync(key);
+            return Ok(new { key = data.ToString() });
+        }
+    }
+}
